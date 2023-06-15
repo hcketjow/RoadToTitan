eaaf733b84889ebeb095a64983cdd004e094b062
Ayaka1156 <115259813+Ayaka1156@users.noreply.github.com>
2023-06-15 18:54:18 +0200

    Update Wind.java

--- Wind.java
+++ Wind.java
@@ -16,5 +16,16 @@ class Wind {
         return wind;
     }
 
+    public double[] weibull(double eta, double beta)
+    {
+        double[] wind = new double[3];
+        for (int i = 1; i < 3; i++) {
+            wind[i] = new WeibullDistribution(getRandomGenerator(), shape, scale,
+                    WeibullDistribution.DEFAULT_INVERSE_ABSOLUTE_ACCURACY).sample();
+        }
+
+        return wind;
+    }
+
 
 }
