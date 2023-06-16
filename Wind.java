5a6a20bb242ccf03cb4e77ad714e5bcec9d38bf9
Ayaka Minamii <minaiiayaka@minaiayakanoMacBook-Air.local>
2023-06-16 10:57:25 +0200

    add_wind

--- Wind.java
+++ Wind.java
@@ -1,31 +1,1 @@
-import java.io.*;
-import java.util.*;
-
-class Wind {
-    public static double[] SDN() //standard normal distribution
-    {
-        double[] wind = new double[3];
-        Random ran = new Random();
-        double min = -1;
-        double max = 1;
-
-        for (int i = 1; i < 3; i++) {
-            wind[i] = min + (max - min) * ran.nextGaussian();
-        }
-
-        return wind;
-    }
-
-    public double[] weibull(double eta, double beta)
-    {
-        double[] wind = new double[3];
-        for (int i = 1; i < 3; i++) {
-            wind[i] = new WeibullDistribution(getRandomGenerator(), shape, scale,
-                    WeibullDistribution.DEFAULT_INVERSE_ABSOLUTE_ACCURACY).sample();
-        }
-
-        return wind;
-    }
-
-
-}
+import java.io.*;import java.util.*;class Wind {    public static double[] SDN() //standard normal distribution    {        double[] wind = new double[3];        Random ran = new Random();        double min = -1;        double max = 1;        for (int i = 1; i < 3; i++) {            wind[i] = min + (max - min) * ran.nextGaussian();        }        return wind;    }    public double[] weibull(double eta, double beta)    {        double[] wind = new double[3];        for (int i = 1; i < 3; i++) {            wind[i] = new WeibullDistribution(getRandomGenerator(), shape, scale,                    WeibullDistribution.DEFAULT_INVERSE_ABSOLUTE_ACCURACY).sample();        }        return wind;    }}
