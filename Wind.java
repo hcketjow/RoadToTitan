8e1c3b5bd518d8bae6d0d1934f819f8a55482052
Ayaka1156 <115259813+Ayaka1156@users.noreply.github.com>
2023-06-16 12:41:32 +0200

    Update Wind.java

--- Wind.java
+++ Wind.java
@@ -1,1 +1,41 @@
-import java.io.*;import java.util.*;class Wind {    public static double[] SDN() //standard normal distribution    {        double[] wind = new double[3];        Random ran = new Random();        double min = -1;        double max = 1;        for (int i = 1; i < 3; i++) {            wind[i] = min + (max - min) * ran.nextGaussian();        }        return wind;    }    public double[] weibull(double eta, double beta)    {        double[] wind = new double[3];        for (int i = 1; i < 3; i++) {            wind[i] = new WeibullDistribution(getRandomGenerator(), shape, scale,                    WeibullDistribution.DEFAULT_INVERSE_ABSOLUTE_ACCURACY).sample();        }        return wind;    }}
+import java.util.Random;
+
+class Wind {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static double[] SDN() //standard normal distribution
+    {
+        double[] wind = new double[3];
+        Random ran = new Random();
+        double min = -1;
+        double max = 1;
+
+        for (int i = 1; i < 3; i++) {
+            wind[i] = min + (max - min) * ran.nextGaussian();
+        }
+
+        return wind;
+    }
+
+    public static double generateWeibullRandom(double shape, double scale) {
+        Random random = new Random();
+        double u = random.nextDouble(); // Uniform random value between 0 and 1
+
+        // Inverse transform sampling using the Weibull distribution formula
+        double x = scale * Math.pow(-Math.log(1 - u), 1 / shape);
+        return x;
+    }
+
+    public static double WeibullRandomGenerator() {
+        double shape = 2.5; // Shape parameter
+        double scale = 1.0; // Scale parameter
+        double randomValue = generateWeibullRandom(shape, scale);
+
+        return randomValue;
+    }
+
+
+}
