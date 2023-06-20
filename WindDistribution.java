8ee43dae6a3cdcdd44c95e29d892634108a8545d
Ayaka1156 <minamii3219@gmail.com>
2023-06-20 10:51:35 +0200

    Deleted complicated ones

--- WindDistribution.java
+++ WindDistribution.java
@@ -12,15 +12,7 @@ class WindDistribution {
         }
         System.out.println("Normal Distribution");
         for (int i = 0; i < 5; i++) {
-            System.out.println(Arrays.toString(SDN()));
-        }
-        System.out.println("Weibull Distribution");
-        for (int i = 0; i < 5; i++) {
-            System.out.println(Arrays.toString(weibull()));
-        }
-        System.out.println("Rayleigh Distribution");
-        for (int i = 0; i < 5; i++) {
-            System.out.println(Arrays.toString(rayleigh()));
+            System.out.println(Arrays.toString(SDN(0.5,0)));
         }
     }
 
@@ -30,7 +22,7 @@ class WindDistribution {
         Random random = new Random();
 
         // Define the range of the random variable
-        double minValue = 0.0;
+        double minValue = -1.0;
         double maxValue = 1.0;
 
         // Generate a random value within the range
@@ -41,68 +33,17 @@ class WindDistribution {
 
         return wind;
     }
-    public static double[] SDN() //standard normal distribution
+    public static double[] SDN(double standardDeviation, double mean) //standard normal distribution
     {
         double[] wind = new double[3];
         Random ran = new Random();
 
-        for (int i = 1; i < 3; i++) {
-            wind[i] = ran.nextGaussian();
-        }
-
-        return wind;
-    }
-
-    public static double generateWeibullRandom(double shape, double scale) {
-        Random ran = new Random();
-        double min = -1.0;
-        double max = 1.0;
-
-        double u = min + (max - min) * ran.nextDouble(); // Uniform random value between -1 and 1
-
-        // Inverse transform sampling using the Weibull distribution formula
-        double x = scale * Math.pow(-Math.log(1 - u), 1 / shape);
-        return x;
-    }
-
-    public static double[] weibull() { //error
-        double shape = 2.5; // Shape parameter
-        double scale = 1.0; // Scale parameter
-        double[] wind = new double[3];
-
         for (int i = 0; i < 3; i++) {
-            wind[i] = generateWeibullRandom(shape, scale);
-        }
-        return wind;
-    }
-
-
-
-    private static double[] rayleigh(double scale) { //error
-        Random random = new Random();
-        double[] wind = new double[3];
-
-        double u = random.nextDouble();
-        double rayleighValue =
-
-
-        double sigma = (max - min) / Math.sqrt(Math.PI / 2.0);
-
-        for (int i = 0; i < 3; i++) {
-            // Generate a random number with Gaussian distribution
-            double gaussianValue = random.nextGaussian();
-
-            // Apply Rayleigh distribution formula to transform the Gaussian number
-            double rayleighValue = sigma * Math.sqrt(-2.0 * Math.log(gaussianValue));
-
-            // Scale and shift the value to the desired range
-            double scaledValue = min + (max - min) * (rayleighValue + 1.0) / 2.0;
-            wind[i] = scaledValue;
+            wind[i] = ran.nextGaussian() * standardDeviation + mean;
+            // r.nextGaussian()*desiredStandardDeviation+desiredMean;
         }
 
         return wind;
     }
-
-
 
 }
