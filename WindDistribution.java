c11479a17412d02ec271f4a4d9e1b2d47ef7c631
Ayaka1156 <minamii3219@gmail.com>
2023-06-20 10:58:47 +0200

    Fixed bugs

--- WindDistribution.java
+++ WindDistribution.java
@@ -8,7 +8,7 @@ class WindDistribution {
         System.out.println("Just for testing");
         System.out.println("Flat Distribution");
         for (int i = 0; i < 5; i++) {
-            System.out.println(Arrays.toString(flat()));
+            System.out.println(Arrays.toString(flat(-1.0, 1.0)));
         }
         System.out.println("Normal Distribution");
         for (int i = 0; i < 5; i++) {
@@ -16,23 +16,20 @@ class WindDistribution {
         }
     }
 
-    public static double[] flat() //flat distribution, returns a value between 0 and 1
+    public static double[] flat(double min, double max) //flat distribution, returns a value between 0 and 1
     {
         double[] wind = new double[3];
         Random random = new Random();
 
-        // Define the range of the random variable
-        double minValue = -1.0;
-        double maxValue = 1.0;
-
         // Generate a random value within the range
         for (int i = 0; i < 3; i++) {
-            double randomValue = minValue + (maxValue - minValue) * random.nextDouble();
+            double randomValue = min + (max - min) * random.nextDouble();
             wind[i] = randomValue;
         }
 
         return wind;
     }
+    //https://stackoverflow.com/questions/31754209/can-random-nextgaussian-sample-values-from-a-distribution-with-different-mean
     public static double[] SDN(double standardDeviation, double mean) //standard normal distribution
     {
         double[] wind = new double[3];
